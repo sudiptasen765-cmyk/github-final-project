61b69b4bf7871945e9ff1db59edb7973a8076412
sudiptasen765-cmyk <sudiptasen765@gmail.com>
2026-09-06 14:20:23 +0530

    Add files via upload

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+echo "--------------------------"
+
+read -p "Enter the principal amount: " principal
+read -p "Enter the rate of interest: " rate
+read -p "Enter the time period: " time
+
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest: $simple_interest"
